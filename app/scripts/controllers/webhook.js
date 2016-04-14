d692c32b19603063a75773afaa23cb6990cab126
Yannis Flet-Berliac <yannisfbe@gmail.com>
2016-04-14 14:27:05 +0200

    get webhook

--- app/scripts/controllers/webhook.js
+++ app/scripts/controllers/webhook.js
@@ -8,10 +8,11 @@
  * Controller of the translateMeApp
  */
 angular.module('translateMeApp')
-  .controller('WebhookCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WebhookCtrl', function ($http) {
+    $http.get('/webhook/', function (req, res) {
+	  if (req.query['hub.verify_token'] === '<validation_token>') {
+	    res.send(req.query['hub.challenge']);
+	  }
+	  res.send('Error, wrong validation token');
+	})
   });
