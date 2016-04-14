d1f262e36b32e79e2ac235d2e4a795b262f6753a
Yannis Flet-Berliac <yannisfbe@gmail.com>
2016-04-14 14:43:53 +0200

    webhook

--- app/scripts/controllers/webhook.js
+++ app/scripts/controllers/webhook.js
@@ -10,9 +10,9 @@
 angular.module('translateMeApp')
   .controller('WebhookCtrl', function ($http) {
     $http.get('/webhook/', function (req, res) {
-	  if (req.query['hub.verify_token'] === '<validation_token>') {
+	  if (req.query['hub.verify_token'] === 'EAADzIHgmTZBgBAGyD8bsOshnRF4yVmeho4LlqkE1dLCOAD4z8wUCN01Bzst6WKyTGAcMOJXLkRGRwv3N9CItdwe5VA8DtONmqaXKIkBuG8d4ZAuqU47KbRa7BjU6uXD6JXBd3aeWJHZBxQkjldfe9w7fO8jkz3ZAQYwTeBCqgwZDZD') {
 	    res.send(req.query['hub.challenge']);
 	  }
 	  res.send('Error, wrong validation token');
-	})
+	});
   });
