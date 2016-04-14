75909a661dba9b247193b6eec87ea379ee464f12
Yannis Flet-Berliac <yannisfbe@gmail.com>
2016-04-14 13:15:06 +0200

    fix

--- app/scripts/controllers/webhook.js
+++ app/scripts/controllers/webhook.js
@@ -8,7 +8,7 @@
  * Controller of the translateMeApp
  */
 angular.module('translateMeApp')
-  .controller('MainCtrl', function () {
+  .controller('WebhookCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
