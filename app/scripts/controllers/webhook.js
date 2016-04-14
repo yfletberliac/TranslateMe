82dedafb1f78d9e19243653aa2c3f81a7cdd9b7c
Yannis Flet-Berliac <yannisfbe@gmail.com>
2016-04-14 13:07:35 +0200

    webhook

---
+++ app/scripts/controllers/webhook.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name translateMeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the translateMeApp
+ */
+angular.module('translateMeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
